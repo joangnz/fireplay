7f15630024af13ab7f0259823adf6f60dc9c0ab1
joangnz <joangnz@gmail.com>
2025-05-07 17:06:48 +0200

    Fixed globals CSS for better usage of Tailwind, tweaks to Header, creation of Game Details.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,18 +1,25 @@
-'use client'
+'use client';
 
 import Link from 'next/link';
+import './header.css';
 
 export default function Header() {
     return (
-        <header className="bg-[var(--color-primary)] text-white p-4 shadow-md">
-            <div className="max-w-6xl mx-auto flex justify-between itemscenter">
-                <Link href="/" className="text-xl font-bold trackingwide">Fireplay</Link>
-                <nav className="space-x-4">
-                    <Link href="/favorites"
-                        className="hover:underline">Favoritos</Link>
-                    <Link href="/cart"
-                        className="hover:underline">Carrito</Link>
-                    <Link href="/login" className="hover:underline">Login</Link>
+        <header>
+            <div className="container">
+                <Link href="/" className="text-xl font-bold tracking-wide">
+                    Fireplay
+                </Link>
+                <nav>
+                    <Link href="/favorites" className="hover:underline">
+                        Favoritos
+                    </Link>
+                    <Link href="/cart" className="hover:underline">
+                        Carrito
+                    </Link>
+                    <Link href="/login" className="hover:underline">
+                        Login
+                    </Link>
                 </nav>
             </div>
         </header>
