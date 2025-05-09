75767b096c544336f7d26528eaaf0ab442af1465
joangnz <joangnz@gmail.com>
2025-05-09 19:04:13 +0300

    Styled searchbar

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -29,7 +29,11 @@ export default function Header() {
                         placeholder='Search games...'
                         value={search}
                         onChange={(e) => setSearch(e.target.value)} ></input>
-                    <button id="searchBarSubmit" type="submit">SEARCH</button>
+                    <button id="searchBarSubmit" type="submit">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                            <path d="M10 2a8 8 0 105.293 14.293l4.707 4.707 1.414-1.414-4.707-4.707A8 8 0 0010 2zm0 2a6 6 0 110 12 6 6 0 010-12z" />
+                        </svg>
+                    </button>
                 </form>
                 <nav>
                     <Link href="/favorites" className="hover:underline">
