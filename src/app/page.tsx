c400de88efa206c161e4ce3dac12f8a216ccc02e
joangnz <joangnz@gmail.com>
2025-05-20 19:32:47 +0300

    Get username in the Search and Main pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,27 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
 import { getSearchedGames } from "../lib/rawg";
 import GameCard from "../components/card";
 import { Game } from "@/types/games.types";
 
-export default async function HomePage() {
-  const games = await getSearchedGames("");
+export default function HomePage() {
+  const [games, setGames] = useState<Game[]>([]);
+  const [username, setUsername] = useState<string>("");
+
+  useEffect(() => {
+    const storedUsername = localStorage.getItem('username') || '';
+    setUsername(storedUsername);
+
+    getSearchedGames("", storedUsername).then((fetchedGames) => {
+      setGames(fetchedGames);
+    });
+  }, []);
 
   return (
     <section className="p-8 grid grid-cols-1 md:grid-cols-3 gap-6">
-      {games.map((game: Game) => (
+      {games.map((game) => (
         <GameCard key={game.id} game={game} />
       ))}
     </section>
