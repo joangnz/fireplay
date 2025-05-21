086aec0285c6725247a45158562cf501cd883c13
joangnz <joangnz@gmail.com>
2025-05-21 19:31:23 +0300

    Initialization of cart page

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+export default function Profile() {
+    const [username, setUsername] = useState<string>("");
+
+    useEffect(() => {
+        const storedUsername = localStorage.getItem('username') || '';
+        storedUsername ? setUsername(storedUsername) : window.location.href = '/login';
+    }, []);
+
+
+    return (
+        <section>
+
+        </section>
+    )
+}
