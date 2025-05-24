4c8b0e0175ddc72a33efad9a10bc76c300f834b0
Joan Gonzàlez Àlvarez <joangnz@gmail.com>
2025-05-24 21:05:11 +0200

    Creation of Connection Data Export const

---
+++ src/lib/connection.ts
@@ -0,0 +1,9 @@
+import mysql from "mysql2/promise";
+
+export const connection_data = {
+    host: "127.0.0.1",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "fireplay_db",
+  };
