8dc3d2e3ea4f45687d247b3a22d3d46053a8ed59
Joan Gonzàlez Àlvarez <joangnz@gmail.com>
2025-05-24 21:07:39 +0200

    Finished getPfp, changePfp and changeUsername functionalities

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,31 +3,53 @@
 import { useEffect, useState } from 'react';
 import $ from 'jquery';
 
-import { newPfp } from '@/lib/requests';
+import { changeUsername, newPfp, getPfp } from '@/lib/requests';
 
 import Edit from "../../../public/edit.svg";
 import "@/styles/profile.css";
 
 export default function Profile() {
     const [username, setUsername] = useState<string>("");
+    const [newUsername, setNewUsername] = useState<string>("");
     const [profilePic, setProfilePic] = useState<string>("https://www.lavanguardia.com/peliculas-series/images/profile/2023/2/w300/hugJtkZDUXbKdEWXbl5rz3qrOMQ.jpg");
     const [selectedSection, setSelectedSection] = useState<string>("favoritos");
     const [fileInput, setFileInput] = useState<File | null>(null);
 
     useEffect(() => {
         const storedUsername = localStorage.getItem('username') || '';
-        storedUsername ? setUsername(storedUsername) : window.location.href = '/login';
+        if (storedUsername) {
+            setUsername(storedUsername); setNewUsername(storedUsername)
+        } else window.location.href = '/login';
 
-        const storedProfilePic = localStorage.getItem('profilePic') || profilePic;
-        setProfilePic(storedProfilePic);
+        const fetchPfp = async () => {
+            try {
+                const res = await getPfp(storedUsername);
+                if (res?.data?.route) {
+                    setProfilePic(res.data.route);
+                }
+            } catch (error) {
+                console.error("Failed to fetch profile picture", error);
+            }
+        };
+
+        fetchPfp();
     }, []);
 
     const handleUsernameChange = (e: any) => {
-        const newUsername = $(e.currentTarget).val()
+        setNewUsername($(e.currentTarget).val());
+    }
+
+    const handleUsernameSubmit = async () => {
+        const res = await changeUsername(username, newUsername);
+
+        if (res?.data.success) {
+            localStorage.setItem('username', newUsername);
+            setUsername(newUsername);
+        }
     }
 
     const handleOverlayClick = () => {
-        const fileInputElement = document.getElementById('pfp-file') as HTMLInputElement;
+        const fileInputElement = $('#pfp-file')[0] as HTMLInputElement;
         if (fileInputElement) {
             fileInputElement.click();
         }
@@ -51,7 +73,6 @@ export default function Profile() {
             try {
                 newPfp(formData).then((res: any) => {
                     setProfilePic(res.data.path);
-                    localStorage.setItem('profilePic', res.data.path);
                 });
             } catch (error) {
             }
@@ -61,7 +82,7 @@ export default function Profile() {
 
     return (
         <section className='main-info p-8 w-2/3 m-auto grid grid-cols-1 md:grid-cols-3 gap-6'>
-            <input id="pfp-file" className="hide" type="file" name="pfp-file"  accept='image/jpeg, image/png, image/jpg' onChange={handleFileChange} />
+            <input id="pfp-file" className="hide" type="file" name="pfp-file" accept='image/jpeg, image/png, image/jpg' onChange={handleFileChange} />
             <div id="pfp-wrap">
                 <img src={profilePic} alt="Profile Picture" id="pfp" />
                 <div className="w-full h-full overlay flex" onClick={handleOverlayClick}>
@@ -69,7 +90,7 @@ export default function Profile() {
                 </div>
             </div>
             <div id="username" className="col-span-2">
-                <input className='text-xl font-bold tracking-wide inline' value={username} onChange={handleUsernameChange} /> <Edit className="inline pointer"></Edit>
+                <input className='text-xl font-bold tracking-wide inline' value={newUsername} onChange={handleUsernameChange} /> <Edit className="inline pointer" onClick={handleUsernameSubmit}></Edit>
             </div>
 
             <div className='w-full text-center col-span-3 flex grid grid-cols-3 col-gap-6'>
