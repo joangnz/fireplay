f567d0b35528df75e4af4be91d3932f49db5517f
Joan Gonzàlez Àlvarez <joangnz@gmail.com>
2025-05-25 16:39:15 +0200

    Better styling of addcart btton

--- src/components/add-cart.tsx
+++ src/components/add-cart.tsx
@@ -19,5 +19,5 @@ export default function AddCart({ game_id }: { game_id: number }) {
         addCartGame(username, game_id);
     }
 
-    return <button onClick={handleAddCart}>ADD TO CART</button>
+    return <button onClick={handleAddCart} className="px-6 py-2 mt-4 text-white bg-blue-600 rounded-md shadow hover:bg-blue-700 active:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2 transition-all">ADD TO CART</button>
 }
